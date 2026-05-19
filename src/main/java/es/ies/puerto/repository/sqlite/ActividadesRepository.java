d0c9d2127eac56592403d353b222829e9d5149a3
Jorge <jorcarheroe@gmail.com>
2026-05-19 18:59:49 +0100

    Se desarolla de manera cordianada y con objetivos logrados el avanze del proyecto, mediante el uso proactivo de funciones y metodos avanzados

--- src/main/java/es/ies/puerto/repository/sqlite/ActividadesRepository.java
+++ src/main/java/es/ies/puerto/repository/sqlite/ActividadesRepository.java
@@ -1,40 +1,110 @@
 package es.ies.puerto.repository.sqlite;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import es.ies.puerto.modelo.Actividades;
+import es.ies.puerto.modelo.Usuario;
 import es.ies.puerto.repository.IActividadesRepository;
 
 public class ActividadesRepository implements IActividadesRepository{
 
     @Override
     public boolean save(Actividades actividad) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        try (Connection connection = getConnection();
+            PreparedStatement sentencia = connection.prepareStatement("INSERT INTO actividades (id, nombre, tipo_actividad, duracion, precio, plazas_maximos, plazas_ocupadas) VALUES (?,?,?,?,?,?,?)")){
+            sentencia.setInt(1, actividad.getId());
+            sentencia.setString(2, actividad.getNombre());
+            sentencia.setString(3, actividad.getTipoActividad());
+            sentencia.setInt(4, actividad.getDuracion());
+            sentencia.setInt(5, actividad.getPrecio());
+            sentencia.setInt(6, actividad.getPlazas_maximas());
+            sentencia.setInt(7, actividad.getPlazas_ocupadas());
+            return sentencia.executeUpdate() > 0;
+        } catch (Exception e) {
+            System.err.println("Error creando usuario");
+            return false;
+        }
     }
 
     @Override
     public boolean update(Actividades actividad) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        try (Connection connection = getConnection();
+            PreparedStatement sentencia = connection.prepareStatement("UPDATE actividades SET nombre = ?, tipo_actividad = ?, duracion = ?, precio = ?, plazas_maximos = ?, plazas_ocupadas = ?")){
+            sentencia.setInt(1, actividad.getId());
+            sentencia.setString(2, actividad.getNombre());
+            sentencia.setString(3, actividad.getTipoActividad());
+            sentencia.setInt(4, actividad.getDuracion());
+            sentencia.setInt(5, actividad.getPrecio());
+            sentencia.setInt(6, actividad.getPlazas_maximas());
+            sentencia.setInt(7, actividad.getPlazas_ocupadas());
+            return sentencia.executeUpdate() > 0;
+        } catch (Exception e) {
+            System.err.println("Error creando usuario");
+            return false;
+        }
     }
 
     @Override
     public Actividades findById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        Actividades actividad = null;
+        try (Connection connection = getConnection();
+        PreparedStatement setencia = connection.prepareStatement("SELECT * FROM actividades WHERE id =?")){
+            setencia.setInt(1, id);
+            ResultSet resultado = setencia.executeQuery();
+            while (resultado.next()) {
+                String nombre = resultado.getString("nombre");
+                String tipoActividad = resultado.getString("tipo_actividad");
+                int duracion = resultado.getInt("duracion");
+                int precio = resultado.getInt("precio");
+                String plazas_maximas = resultado.getString("plazas_maximas");
+                String plazas_ocupadas = resultado.getString("plazas_ocupadas");
+                actividad = new Actividades(id, nombre, tipoActividad, duracion, precio, duracion, precio);
+            }
+            return actividad;
+        } catch (Exception e) {
+            System.err.println("Error buscando los usuarios");
+            return null;
+        }
     }
 
     @Override
     public List<Actividades> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        List<Actividades> actividades = new ArrayList<>();
+        try (Connection connection = getConnection();
+        PreparedStatement setencia = connection.prepareStatement("SELECT * FROM actividades")){
+            ResultSet resultado = setencia.executeQuery();
+            while (resultado.next()) {
+                int id = resultado.getInt("id");
+                String nombre = resultado.getString("nombre");
+                String tipoActividad = resultado.getString("tipo_actividad");
+                int duracion = resultado.getInt("duracion");
+                int precio = resultado.getInt("precio");
+                String plazas_maximas = resultado.getString("plazas_maximas");
+                String plazas_ocupadas = resultado.getString("plazas_ocupadas");
+                actividades.add(new Actividades(id, nombre, tipoActividad, duracion, precio, duracion, precio));
+            }
+            return actividades;
+            
+        } catch (Exception e) {
+            System.err.println("Error buscando los usuarios");
+            return null;
+        }
     }
 
     @Override
     public boolean delete(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        try (Connection connection = getConnection();
+        PreparedStatement setencia = connection.prepareStatement("DELETE * FROM actividades WHERE id =?")){
+            setencia.setInt(1, id);
+            return setencia.executeUpdate() == 1;
+        } catch (Exception e) {
+            System.err.println("Error al eliminar el usuario");
+            return false;
+        }
     }
 
 }
--- src/main/java/es/ies/puerto/repository/sqlite/IncidenciasRepository.java
+++ src/main/java/es/ies/puerto/repository/sqlite/IncidenciasRepository.java
@@ -1,16 +1,33 @@
 package es.ies.puerto.repository.sqlite;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import es.ies.puerto.modelo.Incidencias;
 import es.ies.puerto.repository.IIncidenciasRepository;
 
 public class IncidenciasRepository implements IIncidenciasRepository{
+    
+    public IncidenciasRepository(){
+        super();
+    }
 
     @Override
     public boolean save(Incidencias incidencia) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        try (Connection connection = getConnection();
+            PreparedStatement sentencia = connection.prepareStatement("INSERT INTO incidencias (id, id_usuario, asunto, descripcion, fecha, estado) VALUES (?,?,?,?,?,?)")){
+            sentencia.setInt(1, incidencia.getIdIncidencia());
+            sentencia.setInt(2, incidencia.getId());
+            sentencia.setString(3, incidencia.getAsunto());
+            sentencia.setString(4, incidencia.getDescripcion());
+            sentencia.setString(5, incidencia.getFecha());
+            sentencia.setString(6, incidencia.getAsunto());
+            return sentencia.executeUpdate() > 0;
+        } catch (Exception e) {
+            System.err.println("Error creando incidencia");
+            return false;
+        }
     }
 
     @Override
