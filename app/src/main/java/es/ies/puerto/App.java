de6173a84fd10fa6fcdef296d990d3b6856f513e
HectorPoleo <hectorpoleo2007@gmail.com>
2026-06-11 00:16:24 +0100

    Se adapta el apartado visual del javafx

--- app/src/main/java/es/ies/puerto/App.java
+++ app/src/main/java/es/ies/puerto/App.java
@@ -1,19 +1,20 @@
 package es.ies.puerto;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/es/ies/puerto/view/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 390, 744);
         stage.setTitle("CentroPlus Connect");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
